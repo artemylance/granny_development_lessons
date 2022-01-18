745512de880b9bd7d17db27b5c4108f392264ca4
Artemy Lance <artemylance@gmail.com>
2022-01-18 17:17:52 +0300

    test commit

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+
+const bodyCStyle = window.getComputedStyle(body);
+
+// body.innerText = `font ${bodyCStyle.font}\n line-height: ${bodyCStyle.lineHeight}`;
+// body.style.fontSize = '62.5%';
+
+// body.innerHTML = window.location;
